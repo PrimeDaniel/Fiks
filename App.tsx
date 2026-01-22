6dc66e0db171e1be4197f40a5ba7e53bdebe1a11
Daniel Fraimovich <128545450+PrimeDaniel@users.noreply.github.com>
2026-01-23 01:43:40 +0200

    feat: Integrate theme provider in App

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
 import { I18nProvider } from './src/i18n';
+import { ThemeProvider } from './src/theme/ThemeContext';
 import {
     useFonts as usePoppins,
     Poppins_400Regular,
@@ -43,12 +44,15 @@ export default function App() {
     }
 
     return (
-        <I18nProvider>
-            <SafeAreaProvider>
-                <NavigationContainer>
-                    <AppNavigator />
-                </NavigationContainer>
-            </SafeAreaProvider>
-        </I18nProvider>
+        <ThemeProvider>
+            <I18nProvider>
+                <SafeAreaProvider>
+                    <NavigationContainer>
+                        <AppNavigator />
+                    </NavigationContainer>
+                </SafeAreaProvider>
+            </I18nProvider>
+        </ThemeProvider>
     );
 }
+
